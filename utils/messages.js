43b41fb2134a3df5b0e6124a1e1e76c579938cab
Van Hau <hn4901298@gmail.com>
2023-04-28 22:54:18 +0700

    ChatRealtime

---
+++ utils/messages.js
@@ -0,0 +1,14 @@
+// dependencies
+const moment = require("moment-timezone");
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    // time: moment().format('h:mm a')
+
+    time: moment().tz("Asia/HaNoi").format("h:mm a"),
+  };
+}
+
+module.exports = formatMessage;
